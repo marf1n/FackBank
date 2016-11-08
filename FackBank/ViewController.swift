42c6ca7ed077f2d3b2b78f0a216d3f52c5d91d73
Vladyslav Filippov <VadyslavFilippov@MacBook-Air-Vladyslav.local>
2016-11-08 20:58:50 +0200

    Initial Commit

---
+++ FackBank/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FackBank
+//
+//  Created by Vladyslav Filippov on 08.11.16.
+//  Copyright © 2016 Vladyslav Filippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  
+
+
+}
+
